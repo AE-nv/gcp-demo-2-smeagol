09b388742db6bd6921a6bc6d8bc97cb99d411fe2
Nils Lamot <nils.lamot@ravago.com>
2017-12-01 22:41:31 +0100

    Gollum

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -3,9 +3,9 @@ USE smeagol;
 INSERT IGNORE INTO quotes VALUES (1, 'My precious');
 INSERT IGNORE INTO quotes VALUES (2, 'They took it from us');
 
-use gollom;
+use gollum;
 
 INSERT IGNORE INTO quotes VALUES (1, 'Filthy hobbitses!');
-INSERT IGNORE INTO quotes VALUES (2, 'GOLLOM, GOLLOM!');
+INSERT IGNORE INTO quotes VALUES (2, 'GOLLUM, GOLLUM!');
 
 use smeagol;
